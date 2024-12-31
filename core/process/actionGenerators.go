a17068fb1bb019652caaab3692f5740a5af89e19
ssq0-0 <snikers1kitkat2@gmail.com>
2025-01-01 02:50:48 +0300

    Replace generate and helpers logic in other files

---
+++ core/process/actionGenerators.go
@@ -0,0 +1,190 @@
+package process
+
+import (
+	"fmt"
+	"lisk/account"
+	"lisk/ethClient"
+	"lisk/globals"
+	"lisk/logger"
+	"lisk/models"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+var actionGenerators = map[string]func(acc *account.Account, clients map[string]*ethClient.Client) (ActionProcess, error){
+	"Oku":                generateSwap,
+	"Ionic":              generateLiquidity,
+	"Relay":              generateBridgeToLisk,
+	"Checker":            generateChecker,
+	"Portal_daily_check": generateDailyCheck,
+	"Portal_main_tasks":  generateMainTasks,
+}
+
+func generateChecker(acc *account.Account, clients map[string]*ethClient.Client) (ActionProcess, error) {
+	return ActionProcess{TypeAction: globals.Checker, Module: "Checker"}, nil
+}
+func generateDailyCheck(acc *account.Account, clients map[string]*ethClient.Client) (ActionProcess, error) {
+	return ActionProcess{TypeAction: globals.DailyCheck, Module: "Portal"}, nil
+}
+
+func generateMainTasks(acc *account.Account, clients map[string]*ethClient.Client) (ActionProcess, error) {
+	return ActionProcess{TypeAction: globals.MainTasks, Module: "Portal"}, nil
+}
+
+func generateSwap(acc *account.Account, clients map[string]*ethClient.Client) (ActionProcess, error) {
+	if len(acc.LastSwaps) > 1 {
+		last := acc.LastSwaps[len(acc.LastSwaps)-1]
+		prev := acc.LastSwaps[len(acc.LastSwaps)-2]
+		if last.TokenFrom == prev.TokenFrom && last.TokenTo == prev.TokenTo {
+			return ActionProcess{TypeAction: globals.Unknown},
+				fmt.Errorf("two identical swaps in a row not allowed")
+		}
+	}
+
+	var tokenFrom common.Address
+	if len(acc.LastSwaps) == 0 {
+		tokenFrom = globals.WETH
+	} else {
+		lastTokenTo := acc.LastSwaps[len(acc.LastSwaps)-1].TokenTo
+		if lastTokenTo != globals.LISK {
+			tokenFrom = lastTokenTo
+		} else {
+			tokenFrom = globals.WETH
+		}
+	}
+
+	var tokenTo common.Address
+	const maxAttempts = 5
+	for attempts := 0; attempts < maxAttempts; attempts++ {
+		tokenTo = selectDifferentToken(tokenFrom)
+		if !excludeSwap(tokenFrom, tokenTo) {
+			break
+		}
+		if attempts == maxAttempts-1 {
+			return ActionProcess{TypeAction: globals.Unknown}, fmt.Errorf("failed to select non-excluded token after %d attempts", maxAttempts)
+		}
+	}
+	acc.LastSwaps = append(acc.LastSwaps, models.SwapPair{
+		TokenFrom: tokenFrom,
+		TokenTo:   tokenTo,
+	})
+
+	amount, err := canDoActionByBalance(tokenFrom, acc, clients["lisk"])
+	if err != nil {
+		return ActionProcess{TypeAction: globals.Unknown}, err
+	}
+
+	return ActionProcess{
+		TokenFrom:  tokenFrom,
+		TokenTo:    tokenTo,
+		Amount:     amount,
+		TypeAction: globals.Swap,
+		Module:     "Oku",
+	}, nil
+}
+
+func generateLiquidity(acc *account.Account, clients map[string]*ethClient.Client) (ActionProcess, error) {
+	liqState := acc.LiquidityState
+
+	if liqState.ActionCount == 0 {
+		action := ActionProcess{
+			TokenFrom:  globals.USDT,
+			Amount:     big.NewInt(9e5),
+			TypeAction: globals.Supply,
+			Module:     "Ionic",
+		}
+		updateLiquidityState(acc, action.TypeAction)
+		return action, nil
+	}
+
+	if liqState.ActionCount == 1 {
+		action := ActionProcess{
+			TypeAction: globals.EnterMarket,
+			Module:     "Ionic",
+		}
+		updateLiquidityState(acc, action.TypeAction)
+		return action, nil
+	}
+
+	if liqState.LastAction == globals.Redeem {
+		if !liqState.PendingEnterAfterWithdraw {
+			liqState.PendingEnterAfterWithdraw = true
+			action := ActionProcess{
+				TokenFrom:  globals.USDT,
+				Amount:     big.NewInt(1e5),
+				TypeAction: globals.Supply,
+				Module:     "Ionic",
+			}
+			updateLiquidityState(acc, action.TypeAction)
+			return action, nil
+		} else {
+			liqState.PendingEnterAfterWithdraw = false
+			action := ActionProcess{
+				TypeAction: globals.EnterMarket,
+				Module:     "Ionic",
+			}
+			updateLiquidityState(acc, action.TypeAction)
+			return action, nil
+		}
+	}
+	if liqState.LastAction == globals.Borrow {
+		action := ActionProcess{
+			TokenFrom:  globals.LISK,
+			Amount:     big.NewInt(1e17),
+			TypeAction: globals.Repay,
+			Module:     "Ionic",
+		}
+		updateLiquidityState(acc, action.TypeAction)
+		return action, nil
+	}
+
+	if liqState.LastAction == globals.Repay || liqState.LastAction == globals.EnterMarket {
+		action := ActionProcess{
+			TokenFrom:  globals.LISK,
+			Amount:     big.NewInt(2e17),
+			TypeAction: globals.Borrow,
+			Module:     "Ionic",
+		}
+		updateLiquidityState(acc, action.TypeAction)
+		return action, nil
+	}
+
+	if liqState.LastAction == globals.Supply {
+		action := ActionProcess{
+			TokenFrom:  globals.USDT,
+			Amount:     big.NewInt(1e4),
+			TypeAction: globals.Supply,
+			Module:     "Ionic",
+		}
+		updateLiquidityState(acc, action.TypeAction)
+		return action, nil
+	}
+
+	return ActionProcess{TypeAction: globals.Unknown}, nil
+}
+
+func generateBridgeToLisk(acc *account.Account, clients map[string]*ethClient.Client) (ActionProcess, error) {
+	chain, balance, err := getMaxBalance(acc, clients)
+	if err != nil {
+		return ActionProcess{TypeAction: globals.Unknown}, fmt.Errorf("failed get max balance in all chains: %v", err)
+	}
+
+	if chain == "" {
+		return ActionProcess{TypeAction: globals.Unknown}, fmt.Errorf("no balance in other networks")
+	}
+	logger.GlobalLogger.Infof("Bridge to LISK. From: %s.", chain)
+	return bridgeToLisk(acc, balance, chain, clients[chain])
+}
+
+func bridgeToLisk(acc *account.Account, balance *big.Int, chain string, client *ethClient.Client) (ActionProcess, error) {
+	percentBalance := new(big.Int).Mul(balance, big.NewInt(70))
+	percentBalance.Div(percentBalance, big.NewInt(100))
+
+	return ActionProcess{
+		TokenFrom:  globals.NATIVE,
+		TypeAction: globals.Bridge[chain],
+		Amount:     percentBalance,
+		Module:     "Relay",
+	}, nil
+}
---
+++ core/process/helpers.go
@@ -0,0 +1,148 @@
+package process
+
+import (
+	"fmt"
+	"lisk/account"
+	"lisk/ethClient"
+	"lisk/globals"
+	"math/big"
+	"math/rand"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func generateTimeWindow(totalTime, actionCount int) []time.Duration {
+	if actionCount <= 0 {
+		return nil
+	}
+
+	baseInterval := time.Duration(totalTime) * time.Minute / time.Duration(actionCount)
+	const variationFactor = 0.2
+	const minVariation = time.Second
+	const maxVariation = 30 * time.Second
+
+	intervals := make([]time.Duration, 0, actionCount)
+	for i := 0; i < actionCount; i++ {
+		variation := float64(baseInterval) * variationFactor
+
+		if variation < float64(minVariation) {
+			variation = float64(minVariation)
+		} else if variation > float64(maxVariation) {
+			variation = float64(maxVariation)
+		}
+
+		randomVariation := time.Duration(rand.Float64()*2*variation - variation)
+		interval := baseInterval + randomVariation
+
+		if interval < 0 {
+			interval = baseInterval
+		}
+
+		intervals = append(intervals, interval)
+	}
+
+	return intervals
+}
+
+func getMaxBalance(acc *account.Account, clients map[string]*ethClient.Client) (string, *big.Int, error) {
+	var (
+		maxChain string
+		maxBal   = big.NewInt(0)
+	)
+
+	for chain, client := range clients {
+		if chain == "lisk" {
+			continue
+		}
+
+		balance, err := client.BalanceCheck(acc.Address, globals.WETH)
+		if err != nil {
+			return "", nil, fmt.Errorf("getMaxBalance: failed in chain %s: %w", chain, err)
+		}
+
+		if balance.Cmp(maxBal) > 0 {
+			maxBal.Set(balance)
+			maxChain = chain
+		}
+	}
+
+	return maxChain, maxBal, nil
+}
+
+func generateNextAction(acc *account.Account, availableModules []string, clients map[string]*ethClient.Client) (ActionProcess, error) {
+	if len(availableModules) == 0 {
+		return ActionProcess{TypeAction: globals.Unknown}, fmt.Errorf("no available modules for account %s", acc.Address.Hex())
+	}
+
+	selectedModule := availableModules[rand.Intn(len(availableModules))]
+
+	generator, exists := actionGenerators[selectedModule]
+	if !exists {
+		return ActionProcess{TypeAction: globals.Unknown}, fmt.Errorf("no action generator for module '%s'", selectedModule)
+	}
+
+	return generator(acc, clients)
+}
+
+func selectDifferentToken(tokenFrom common.Address) common.Address {
+	tokens := []common.Address{
+		globals.LISK,
+		globals.USDT,
+		globals.USDC,
+		globals.WETH,
+	}
+
+	for attempts := 0; attempts < 10; attempts++ {
+		t := tokens[rand.Intn(len(tokens))]
+		if t != tokenFrom {
+			return t
+		}
+	}
+
+	return globals.USDT
+}
+
+func excludeSwap(tokenFrom, tokenTo common.Address) bool {
+	excludedPairs := map[common.Address]map[common.Address]bool{
+		globals.USDT: {
+			globals.LISK: true,
+		},
+	}
+
+	if tos, exists := excludedPairs[tokenFrom]; exists {
+		if _, excluded := tos[tokenTo]; excluded {
+			return true
+		}
+	}
+	return false
+}
+
+func canDoActionByBalance(token common.Address, acc *account.Account, client *ethClient.Client) (*big.Int, error) {
+	balance, err := client.BalanceCheck(acc.Address, token)
+	if err != nil {
+		return nil, fmt.Errorf("canDoActionByBalance: balance check failed for %s: %w", acc.Address.Hex(), err)
+	}
+
+	if !checkMinimalAmount(balance, token) {
+		return nil, fmt.Errorf("canDoActionByBalance: balance too low for token %s, account %s",
+			token.Hex(), acc.Address.Hex())
+	}
+
+	return calculateAmount(balance, acc.BalancePercentUsage)
+}
+
+func checkMinimalAmount(balance *big.Int, token common.Address) bool {
+	return balance.Cmp(globals.MinBalances[token]) >= 0
+}
+
+func calculateAmount(amount *big.Int, percent int) (*big.Int, error) {
+	percentAmount := new(big.Int).Mul(amount, big.NewInt(int64(percent)))
+	percentAmount.Div(percentAmount, big.NewInt(100))
+	return percentAmount, nil
+}
+
+func updateLiquidityState(acc *account.Account, actionType globals.ActionType) {
+	acc.LiquidityState.LastAction = actionType
+	acc.LiquidityState.ActionCount++
+}
