30cf55fd639ee8b4ea8fc5b9b4a9a51104f2a27b
ssq0-0 <snikers1kitkat2@gmail.com>
2025-01-05 22:15:50 +0300

    New configure structure. Added instructions for configuring directly in the file. Added fields in config for more flexible programme configuration

--- config/config.go
+++ config/config.go
@@ -6,14 +6,21 @@ import (
 )
 
 type Config struct {
-	ActionCounts   int               `json:"actions_count"`
-	MaxActionsTime int               `json:"max_actions_time"`
-	RPC            map[string]string `json:"rpc"`
-	ABIs           map[string]string `json:"abis"`
-	TokenAddresses map[string]string `json:"token_addresses"`
-	OkuAddresses   map[string]string `json:"oku_addresses"`
-	IonicAddresses map[string]string `json:"ionic_addresses"`
-	Endpoints      map[string]string `json:"enpoints"`
+	StartDate        string            `json:"start_date"`
+	ActionCounts     int               `json:"actions_count"`
+	MaxActionsTime   int               `json:"max_actions_time"`
+	IonicBorrow      string            `json:"ionic_borrow_amount"`
+	IonicSupply      string            `json:"ionic_supply_amount"`
+	AttentionGwei    string            `json:"attention_gwei"`
+	AttentionTime    int               `json:"attention_time_cycle"`
+	MaxAttentionTime int               `json:"max_attention_time"`
+	StateFile        string            `json:"state_file"`
+	RPC              map[string]string `json:"rpc"`
+	ABIs             map[string]string `json:"abis"`
+	TokenAddresses   map[string]string `json:"token_addresses"`
+	OkuAddresses     map[string]string `json:"oku_addresses"`
+	IonicAddresses   map[string]string `json:"ionic_addresses"`
+	Endpoints        map[string]string `json:"enpoints"`
 }
 
 func LoadConfig(path string) (*Config, error) {
